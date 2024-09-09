234dd15ad40350e7f74a6d303800ec8d6238c66e
Galina Netishina <netishinagalina@yandex.ru>
2024-09-09 21:20:10 +0300

    partial rework

--- linked_list.py
+++ linked_list.py
@@ -1,42 +1,53 @@
+from typing import Optional
+
+
 class ObjList:
+    '''Object, which keeps data(string) and references to prev and next objects, if they exist'''
     __slots__ = ['__prev', '__next', '__data']
 
     def __init__(self, data: str):
         self.__data = data
-        self.__prev = None
-        self.__next = None
+        self.__prev: Optional["ObjList"] = None
+        self.__next: Optional["ObjList"] = None
 
     @property
-    def next(self):
+    def next(self) -> "ObjList":
         return self.__next
 
     @next.setter
-    def next(self, other):
+    def next(self, other: "ObjList") -> None:
         self.__next = other
 
     @property
-    def prev(self):
+    def prev(self) -> "ObjList":
         return self.__prev
 
     @prev.setter
-    def prev(self, other):
+    def prev(self, other: "ObjList") -> None:
         self.__prev = other
 
     @property
-    def data(self):
+    def data(self) -> str:
         return self.__data
 
     @data.setter
-    def data(self, data: str):
+    def data(self, data: str) -> None:
         self.__data = data
+
+    def __repr__(self):
+        return self.data
 
 
 class LinkedList:
+    '''represents collection LinkedList of objects of class ObjList,has head and tail attributes, and includes next methods:
+     - add_obj(obj) add to end
+      - remove() - delete last element
+      - get_data() - return list of data from all ObjList in collection'''
     def __init__(self):
-        self.head = None
-        self.tail = None
+        self.head: Optional["ObjList"] = None
+        self.tail: Optional["ObjList"] = None
 
-    def add_obj(self, obj: ObjList):
+    def add_obj(self, obj: ObjList) -> None:
         if self.head is None:
             self.head = obj
         else:
@@ -44,7 +55,7 @@ class LinkedList:
             obj.prev = self.tail
         self.tail = obj
 
-    def remove_obj(self):
+    def remove_obj(self) -> None:
         if self.head is None:
             raise IndexError('list is empty')
         if tail := self.tail.prev:
@@ -53,11 +64,11 @@ class LinkedList:
         else:
             self.head = None
 
-    def get_data(self):
+    def get_data(self) -> list[ObjList.data]:
         res = []
         obj = self.head
         while obj:
-            res.append(obj.data)
+            res.append(obj)
             obj = obj.next
         return res
 
--- local_imitation.py
+++ local_imitation.py
@@ -1,12 +1,6 @@
-class Data:
-    __slots__ = ['data', 'ip']
+from collections import namedtuple, deque
 
-    def __init__(self, data: str, ip: int):
-        self.data = data
-        self.ip = ip
-
-    def __repr__(self):
-        return f"Data {{message: {self.data}, send_to_ip: {self.ip}}}"
+Data = namedtuple('Data', ['text', 'ip'])
 
 
 class Server:
@@ -15,51 +9,52 @@ class Server:
     def __init__(self):
         self.ip: int = self.__class__.counter
         self.__class__.counter += 1
-        self.buffer: list[Data] = []
+        self.buffer: deque[Data] = deque()
         self.router = None
 
-    def send_data(self, data: Data):
+    def send_data(self, data: Data) -> None:
         self.router.buffer.append(data)
 
     def get_data(self):
-        return [self.buffer.pop() for _ in range(len(self.buffer))]
+        while self.buffer:
+            yield self.buffer.popleft().text
 
-    def get_ip(self):
+    def get_ip(self) -> int:
         return self.ip
 
 
 class Router:
     def __init__(self):
         self.servers: dict[int, Server] = {}
-        self.buffer: list[Data] = []
+        self.buffer: deque[Data] = deque()
 
-    def link(self, *servers: Server):
+    def link(self, *servers: list[Server]) -> None:
         for server in servers:
             self.servers[server.ip] = server
             server.router = self
 
-    def unlink(self, server: Server):
-        del self.servers[server.ip]
-        server.router = None
+    def unlink(self, server: Server) -> None:
+        try:
+            del self.servers[server.ip]
+            server.router = None
+        except Exception:
+            raise
 
-    def send_data(self):
+    def send_data(self) -> None:
         while self.buffer:
-            pack = self.buffer.pop()
+            pack = self.buffer.popleft()
             send_to = pack.ip
             self.servers[send_to].buffer.append(pack)
 
 
-# if __name__ == "__main__":
-    # router = Router()
-    # sv_from = Server()
-    # sv_from2 = Server()
-    # router.link(sv_from, sv_from2, Server(), Server())
-    # sv_from.send_data(Data('hello', sv_from2.get_ip()))
-    # sv_from.send_data(Data('hello again', 2))
-    # sv_from2.send_data(Data('hello again', 1))
-    # print('router before: ', router.buffer)
-    # print(sv_from2.buffer, sv_from.buffer)
-    # router.send_data()
-    # print('router after: ', router.buffer)
-    # print('get data from server1: ', sv_from2.get_data())
-    # print(sv_from2.buffer, sv_from.buffer)
+servers = [Server() for _ in range(3)]
+data = [Data(f'message #{i}', i % len(servers)) for i in range(1, 5)]
+r = Router()
+r.link(*servers)
+servers[0].send_data(data[0])
+servers[0].send_data(data[1])
+servers[2].send_data(data[3])
+print(r.buffer)
+r.send_data()
+for s in servers:
+    print(f'server #{s.get_ip()} received: ', *s.get_data())
